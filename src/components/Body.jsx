40ffa6db73638ce13fe947ba727a7312adfe8732
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-05-02 21:26:35 -0400

    :sparkles: playlist body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -41,11 +41,179 @@ export default function Body() {
     };
     getInitialPlaylist();
   }, [token, dispatch]);
+
+  const msToMinutesAndSeconds = (ms) => {
+    var minutes = Math.floor(ms / 60000);
+    var seconds = ((ms % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
+
   return (
     <BodyContainer>
-      <AiFillClockCircle />
+      {selectedPlaylist && (
+        <>
+          <div className="playlist">
+            <div className="image">
+              <img src={selectedPlaylist.image} alt="selectedPlaylist" />
+            </div>
+            <div className="details">
+              <span className="type">PLAYLIST</span>
+              <h1 className="title">{selectedPlaylist.name}</h1>
+              <p className="description">{selectedPlaylist.description}</p>
+            </div>
+          </div>
+          <div className="list">
+            <div className="header__row">
+              <div className="col">
+                <span>#</span>
+              </div>
+              <div className="col">
+                <span>TITLE</span>
+              </div>
+              <div className="col">
+                <span>ALBUM</span>
+              </div>
+              <div className="col">
+                <span>
+                  <AiFillClockCircle />
+                </span>
+              </div>
+            </div>
+            <div className="tracks">
+              {selectedPlaylist.tracks.map(
+                (
+                  {
+                    id,
+                    name,
+                    artists,
+                    image,
+                    duration,
+                    album,
+                    context_uri,
+                    track_number,
+                  },
+                  index
+                ) => {
+                  return (
+                    <div className="row" key={id}>
+                      <div className="col">
+                        <span>{index + 1}</span>
+                      </div>
+                      <div className="col detail">
+                        <img src={image} alt="track" />
+                        <div className="info">
+                          <span className="name">{name}</span>
+                          <span className="artists">{artists}</span>
+                        </div>
+                      </div>
+                      <div className="col">
+                        <span className="album">{album}</span>
+                      </div>
+                      <div className="col">
+                        <span>{msToMinutesAndSeconds(duration)}</span>
+                      </div>
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          </div>
+        </>
+      )}
     </BodyContainer>
   );
 }
 
-const BodyContainer = styled.div``;
+const BodyContainer = styled.div`
+  .playlist {
+    margin: 0 2rem;
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    .image {
+      img {
+        height: 15rem;
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+      }
+    }
+    .details {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      color: #e0dede;
+      .title {
+        color: white;
+        font-size: 4rem;
+      }
+    }
+  }
+  .list {
+    .header__row {
+      display: grid;
+      grid-template-columns: 0.3fr 3fr 2fr 0.5fr;
+      margin: 1rem 0 0 0;
+      color: #dddcdc;
+      position: sticky;
+      top: 15vh;
+      padding: 1rem 3rem;
+      transition: 0.3s ease-in-out;
+      background-color: ${({ headerBackground }) =>
+        headerBackground ? "#000000dc" : "none"};
+    }
+  }
+    .tracks {
+      margin: 0 2rem;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 5rem;
+      .row {
+        padding: 0.5rem 1rem;
+        display: grid;
+        grid-template-columns: 0.3fr 3.1fr 2fr 0.5fr;
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.7);
+        }
+        .col {
+          display: flex;
+          align-items: center;
+          color: #dddcdc;
+          img {
+            height: 40px;
+            width: 40px;
+            cursor:pointer;
+          }
+        }
+        .name{
+          margin-right: 1rem;
+          color: #dddcdc;
+        }
+        .artists{
+          font-size: 13px;
+          color: #ccdc;
+          margin: 0.2rem 0rem;
+        }
+        .name:hover{
+          text-decoration: underline;
+          cursor:pointer;
+        }
+        .artists:hover{
+          text-decoration: underline;
+          cursor:pointer;
+        }
+        .album:hover{
+          text-decoration: underline;
+          cursor:pointer;
+        }
+        .detail {
+          display: flex;
+          flex-direction: row;
+          gap: 1rem;
+          .info {
+            display: flex;
+            flex-direction: column;
+          }
+        }
+      }
+    }
+  }
+`;
